033d9b26de776a321748ae1237cb8c70cf8f8fc4
Imamsubekti26 <imam0subekti@gmail.com>
2024-05-24 01:01:20 +0700

    create story has initiate

---
+++ app/src/main/java/com/imamsubekti/storyapp/view/create/CreateViewModel.kt
@@ -0,0 +1,7 @@
+package com.imamsubekti.storyapp.view.create
+
+import androidx.lifecycle.ViewModel
+import com.imamsubekti.storyapp.repository.DataStoreRepository
+
+class CreateViewModel(private val pref: DataStoreRepository): ViewModel() {
+}
