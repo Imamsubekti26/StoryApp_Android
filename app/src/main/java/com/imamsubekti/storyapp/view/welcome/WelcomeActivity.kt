1f969746a417168193fadc162ad33d23f4efdea6
Imamsubekti26 <imam0subekti@gmail.com>
2024-05-24 00:09:13 +0700

    update welcome screen logic

---
+++ app/src/main/java/com/imamsubekti/storyapp/view/welcome/WelcomeActivity.kt
@@ -0,0 +1,27 @@
+package com.imamsubekti.storyapp.view.welcome
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.imamsubekti.storyapp.databinding.ActivityWelcomeBinding
+import com.imamsubekti.storyapp.ui.LoginActivity
+import com.imamsubekti.storyapp.ui.RegisterActivity
+
+class WelcomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWelcomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.registerButton.setOnClickListener {
+            val toRegister = Intent(this, RegisterActivity::class.java)
+            startActivity(toRegister)
+        }
+
+        binding.loginButton.setOnClickListener {
+            val toRegister = Intent(this, LoginActivity::class.java)
+            startActivity(toRegister)
+        }
+    }
+}
