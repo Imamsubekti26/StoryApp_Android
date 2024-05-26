854fe86e8c4f6dd76ffa37e32fefbf242bbaa635
Imamsubekti26 <imam0subekti@gmail.com>
2024-05-26 10:19:06 +0700

    create animation in welcome screen

--- app/src/main/java/com/imamsubekti/storyapp/view/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/imamsubekti/storyapp/view/welcome/WelcomeActivity.kt
@@ -1,7 +1,10 @@
 package com.imamsubekti.storyapp.view.welcome
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.imamsubekti.storyapp.databinding.ActivityWelcomeBinding
 import com.imamsubekti.storyapp.view.login.LoginActivity
@@ -22,6 +25,21 @@ class WelcomeActivity : AppCompatActivity() {
         binding.loginButton.setOnClickListener {
             val toRegister = Intent(this, LoginActivity::class.java)
             startActivity(toRegister)
+        }
+
+        playAnimation()
+    }
+
+    private fun playAnimation() {
+        val title = ObjectAnimator.ofFloat(binding.title, View.ALPHA, 1f).setDuration(300)
+        val slogan = ObjectAnimator.ofFloat(binding.slogan, View.ALPHA, 1f).setDuration(300)
+        val register = ObjectAnimator.ofFloat(binding.registerButton, View.ALPHA, 1f).setDuration(300)
+        val orText = ObjectAnimator.ofFloat(binding.orText, View.ALPHA, 1f).setDuration(300)
+        val login = ObjectAnimator.ofFloat(binding.loginButton, View.ALPHA, 1f).setDuration(300)
+
+        AnimatorSet().apply {
+            playSequentially(title, slogan, register, orText, login)
+            start()
         }
     }
 }
